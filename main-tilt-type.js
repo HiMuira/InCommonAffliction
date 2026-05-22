4073f1f20ef0ef2f811110002bd52f90e50d9c78
HiMuira <76672242+HiMuira@users.noreply.github.com>
2026-05-21 17:22:14 -0700

    main-tilt-type.js

--- main-tilt-type.js
+++ main-tilt-type.js
@@ -1,30 +1,30 @@
-const pre = document.querySelector("body");
+// const pre = document.querySelector("body");
 
 
-document.addEventListener("mousemove", (e) => {
-    rotateElement(e, pre);
-})
+// document.addEventListener("mousemove", (e) => {
+//     rotateElement(e, pre);
+// })
 
 
-function rotateElement(event, element) {
-    // get mouse position
-    const x = event.clientX;
-    const y = event.clientY;
-    // console.log(x, y)
+// function rotateElement(event, element) {
+//     // get mouse position
+//     const x = event.clientX;
+//     const y = event.clientY;
+//     // console.log(x, y)
 
 
-    // find middle of page
-    const middleX = window.innerWidth / 2;
-    const middleY = window.innerHeight / 2;
+//     // find middle of page
+//     const middleX = window.innerWidth / 2;
+//     const middleY = window.innerHeight / 2;
 
-    // get offset from middle
-    const offsetX = ((x - middleX) / middleX) * 12;
-    const offsetY = ((y - middleY) / middleY) * 12;
-    // console.log(offsetX, offsetY)
+//     // get offset from middle
+//     const offsetX = ((x - middleX) / middleX) * 12;
+//     const offsetY = ((y - middleY) / middleY) * 12;
+//     // console.log(offsetX, offsetY)
 
-    element.style.setProperty("--rotateX", -1 * offsetY + "deg")
-    element.style.setProperty("--rotateY", offsetX + "deg")
-}
+//     element.style.setProperty("--rotateX", -1 * offsetY + "deg")
+//     element.style.setProperty("--rotateY", offsetX + "deg")
+// }
 
 const outputDiv = document.getElementById('typing-container');
 const txt = `// PROJECT NIGHTFALL — BOOT SEQUENCE<nl>
